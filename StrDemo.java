f957abb50d1c38dfce7238f43d97629c6d8ad34c
razauaf <razaraza12514@gmail.com>
2019-03-26 00:09:00 +0500

    secound commit

--- StrDemo.java
+++ StrDemo.java
@@ -1,9 +1,9 @@
-class StrDemo
+class StrDemo 
 {
-public static void main(String args[])
-   {
-	System.out.println( "first line /secound line");
-	System.out.println("A\tB\tC");
-	System.out.println("D\tE\tF");
-    }
- }
+	public static void main (String args []) 
+	{
+	 System.out.println("First line \nSecond line");
+	 System.out.println("A\tB\tC");
+	 System.out.println("D\tE\tF");
+	}
+}
